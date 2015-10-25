51e0c79e42f1000a6fc1954ad3b3dcb58de9aeba
aakashkharche04 <aakashkharche@yahoo.in>
2015-10-25 19:27:07 +0530

    tabletranspose

---
+++ Readme.txt
@@ -0,0 +1,3 @@
+Open the CustomGrid.sln file in Visual Studio 2013 .
+Set TransposeTable.html as the Start up Page
+Run it on google Chrome.
---
+++ TableTranspose/Scripts/Transpose.js
@@ -0,0 +1,57 @@
+﻿var table = document.getElementById("userTbl");
+
+// For Deleting Table Rows
+function DeleteTableRows(tableRef, totalRows) {
+    for (var i = (totalRows - 1 ) ; i >= 0  ; i-- ) {
+        tableRef.deleteRow(i);
+    }
+}
+
+// Returns cell width according to the number to columns
+function ReturnCellWidth(totalCols)
+{
+    var totalWidth = 100;
+    return (totalWidth / totalCols);
+}
+
+//function to add rows in table
+function CreateTable(tableData, totalColumns, totalRows) {
+    if (totalColumns > 0 && totalRows > 0)
+    {
+        var cellWidth = ReturnCellWidth(totalColumns);
+        for (var i = 0; i < totalRows; i++) {
+            var row = table.insertRow(i);
+            for (var j = 0 ; j < totalColumns ; j++) {
+                var cell = row.insertCell(j);
+                cell.width = cellWidth + "%";
+                cell.innerHTML = tableData[j][i];
+            }
+        }
+    }
+}
+
+// Create DataObject that stores the data of the table
+function CreateDataObject(tableRef, totalRows,totalCols) {
+    var dataArray = [];
+    for(var i =0 ; i < totalRows ; i++)
+    {
+        var dataObj = {};
+        for( j = 0 ; j< totalCols ; j++)
+        {
+            dataObj[j] = tableRef.rows[i].cells[j].innerHTML;
+        }
+        dataArray.push(dataObj);
+    }
+    return dataArray;
+}
+
+// called on click of Transpose button
+function TransposeTable(event) {
+    var nRows = table.rows.length;
+    var nCols = table.rows[0].cells.length;
+    var tableData = CreateDataObject(table, nRows, nCols);
+    DeleteTableRows(table, nRows);
+    CreateTable(tableData, nRows, nCols)
+}
+
+
